5d37d07d2a20ceb668b0cea8958b3c25cf848466
Momo <emabr90@gmail.com>
2018-01-16 17:47:11 +0100

    Fixed a mistake in the adimensionalization of eta (macro defining factor pi written wrongly)

--- res_eta.c
+++ res_eta.c
@@ -2,7 +2,7 @@
 
 void Resistive_eta(double *v, double x1, double x2, double x3, double *J, double *eta)
 {
-  double eta0 = 4*M_PI/(CONST_c*CONST_c)*UNIT_VELOCITY*UNIT_LENGTH;/*unit of eta for adimensionalization*/
+  double eta0 = 4*CONST_PI/(CONST_c*CONST_c)*UNIT_VELOCITY*UNIT_LENGTH;/*unit of eta for adimensionalization*/
 
   eta[IDIR] = g_inputParam[ETAX_GAU] / eta0;
   eta[JDIR] = g_inputParam[ETAY_GAU] / eta0;
