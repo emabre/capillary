7caa19a1db2f77ef5aea92fc551973689371d593
Momo <emabr90@gmail.com>
2018-03-31 23:28:08 +0200

    In progress: setting a function for output of current density (debug) in vtk format Biggest part still to do: Write a grid which makes sense for the output of the current density (write_curr_for_deb.c, line 18, 22) (or, alternatively, write some other function to write the vtk header with the grid) (rememeber that J is defined on a shifted grid!!)
    I extended (not tested) the function write_vtk.c which now allows for ouput of a new vector quantity (current density)
    	new file:   write_curr_for_deb.c
    	new file:   write_vtk.c

---
+++ write_curr_for_deb.c
@@ -0,0 +1,43 @@
+#include "pluto.h"
+#include "current_table.h"
+
+void WriteCurrForDebug (char *file_basename, Data *d,
+                        Grid *grid, int dir){
+double unit_J, unit_Mfield;
+char *J_name, *file_name;
+Data_Arr J;
+Grid grid_shifted;
+FILE *fvtk;
+//This variable keeps trace of the number of times this function has been called
+static int curr_debug_idx=-1;
+
+curr_debug_idx++;
+
+if (dir == IDIR) {
+// Create shifted grid accordingly to the direction we're in
+
+}
+if (dir == JDIR) {
+// Create shifted grid accordingly to the direction we're in
+
+}
+
+// Assign current density to Data_Arr J;
+
+
+// Compute the unit of measure
+unit_Mfield = COMPUTE_UNIT_MFIELD(UNIT_VELOCITY, UNIT_DENSITY);
+unit_J = CONST_c/(4*CONST_PI)*unit_Mfield/UNIT_LENGTH;
+
+// Open file, using curr_debug_idx in the name to be sure you do not overwrite other files
+file_name = malloc(strlen(file_basename) + 5 + 4 + 1);
+sprintf(file_name,"%s.%d.vtk", file_basename, curr_debug_idx);
+
+fvtk  = OpenBinaryFile(file_name, SZ_Float_Vect, "w");
+
+// Write J to vtk
+WriteVTK_Header (fvtk, &grid_shifted);
+WriteVTK_Vector (fvtk, J, unit_J, J_name, &grid_shifted);
+
+fclose(fvtk);
+}
