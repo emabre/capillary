88513fc5ad574600ccc249235257f5e8d5e62cf0
Momo <emabr90@gmail.com>
2018-04-30 11:35:16 +0200

    I added a .c file which defines the adi method (now it only says: "Wazaaa!")

---
+++ adi.c
@@ -0,0 +1,8 @@
+/*Functions and utilities for integration of parabolic (currently resistive
+and thermal conduction) terms with the Alternating Directino Implicit algorithm*/
+
+#include "pluto.h"
+
+void ADI(const Data *d, Time_Step *Dts, Grid *grid) {
+    print1("[ADI] Wazaaaaaa!");
+}
--- definitions.h
+++ definitions.h
@@ -16,8 +16,8 @@
 #define  ENTROPY_SWITCH          NO
 #define  DIVB_CONTROL            NO
 #define  BACKGROUND_FIELD        NO
-#define  RESISTIVITY             EXPLICIT
-#define  THERMAL_CONDUCTION      SUPER_TIME_STEPPING
+#define  RESISTIVITY             NO
+#define  THERMAL_CONDUCTION      ALTERNATING_DIRECTION_IMPLICIT
 #define  VISCOSITY               NO
 #define  ROTATING_FRAME          NO
 
