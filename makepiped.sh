b553cf9c00619be2ff9273b220f2b84dc8858d03
Momo <emabr90@gmail.com>
2018-05-07 17:33:38 +0200

    I fixed another mistake in the script makepiped.sh

--- makepiped.sh
+++ makepiped.sh
@@ -2,4 +2,4 @@
 make &> make.out
 # now you can search for warnings/errors inside the output of make.
 # To fin the errors/warning only concerning the files in your directory,
-# use the regex: "[A-Za-z0-9]+\.c" with no quotes, beware the space at the beginning!
+# use the regex: " [A-Za-z0-9]+\.c" with no quotes, beware the space at the beginning!
