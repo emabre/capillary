6bcc99717915eff4ef7b51e4ff94281e8840394e
Momo <emabr90@gmail.com>
2018-06-28 18:56:41 +0200

    I added the current profile as in the old discharge with 95A amperes (peak)

--- current_table.h
+++ current_table.h
@@ -7,7 +7,7 @@
 // from current in Ampere and radius (g_domEnd[0]*UNIT_LENGTH) in cm
 #define BIOTSAV_GAUSS_A_CM(curr_A, rad_cm) (0.2*(curr_A)/(rad_cm))
 
-#define N_CURR_TAB 3
+#define N_CURR_TAB 9
 
 double i_curr_tab[N_CURR_TAB];  /*should I also say extern?*/
 double t_curr_tab[N_CURR_TAB]; /*should I also say extern?*/
