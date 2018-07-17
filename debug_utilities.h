faa503c5c0e9702a1baf92afcd3443977a4ce5c5
Momo <emabr90@gmail.com>
2018-07-17 18:47:54 +0200

    I improved the function to print 4D arrays for debug

--- debug_utilities.h
+++ debug_utilities.h
@@ -2,6 +2,6 @@
 #define DEBUG_UTILITIES
 
 void printmat(double **matrix, int dim1, int dim2);
-void printmat4d(double ****matrix, int dim2, int dim1, int which[4]);
+void printmat4d(double ****matrix, int dim2, int dim1, int which0, int which1, int which2, int which3 );
 
 #endif
