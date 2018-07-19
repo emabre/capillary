4692b8931734521d79560b968cf9f906555ccd2e
Momo <emabr90@gmail.com>
2018-07-19 12:13:06 +0200

    I separated adi.c in two files, adi.c and adi_solvers.c, (the second one contains only the adi schemes and solvers) I also added a realistic current (the old ramp is still there but commented) in current_table.c

--- current_table.c
+++ current_table.c
@@ -2,13 +2,25 @@
 #include "current_table.h"
 
 /*Electrical current table, in Ampere, seconds*/
-// Current as in commit 5e85a9c57...
 double i_curr_tab[N_CURR_TAB] = { 0.0001e+00,
-                                  200.00e+00,
-                                  200.00e+00};
+                                  9.600000e+01,
+                                  1.700000e+02,
+                                  1.400000e+02,
+                                  6.700000e+01,
+                                  1.200000e+01};
 double t_curr_tab[N_CURR_TAB] = { -1.000000e-20,
-                                  200.0e-9,
-                                  2000.000000e-09};
+                                  7.500000e-08,
+                                  2.500000e-07,
+                                  4.650000e-07,
+                                  7.260000e-07,
+                                  1.260000e-06};
+// Current as in commit 5e85a9c57...
+// double i_curr_tab[N_CURR_TAB] = { 0.0001e+00,
+//                                   200.00e+00,
+//                                   200.00e+00};
+// double t_curr_tab[N_CURR_TAB] = { -1.000000e-20,
+//                                   200.0e-9,
+//                                   2000.000000e-09};
 //
 /*------------------------------------------------------
   Function to compute current at the present time step
