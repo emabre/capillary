d2188026e8e8bd1757413728d48e77dd54cf7329
Momo <emabr90@gmail.com>
2018-08-29 18:00:02 +0200

    I changed just one detail in a print function

--- mappersLines.c
+++ mappersLines.c
@@ -47,7 +47,7 @@ void ConsToPrimLines (Data_Arr U, Data_Arr V, unsigned char ***flag, Lines *line
       if (err) {
         print1("[ConsToPrimLines] Error converting Cons->Prim (k:%d,j:%d)", k,j);
         // QUIT_PLUTO(1);
-        print1("\nI move on...");        
+        print1("\nI move on...\n");        
       }
 
       for (i = ibeg; i <= iend; i++) NVAR_LOOP(nv) V[nv][k][j][i] = v[i][nv];
